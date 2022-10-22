08f6ab01192753c76e858513feb51e66d0df7d2c
Joe P Shoulak <joe@squarecompass.io>
2022-10-21 19:19:49 -0500

    sin on screen, installing dat.gui. Looks amazing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { resizeCanvas } from "./lib/helper.js";
+import { Class } from "./lib/Class.js";
 
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("my-canvas");
@@ -8,11 +9,31 @@ const setup = () => {
   resizeCanvas(canvas);
 };
 
+const graph = (curve) => {
+  c.beginPath();
+  c.moveTo(0, innerHeight / 2);
+  for (let x = 0; x < innerWidth; x++) {
+    const y = curve(x);
+
+    c.lineTo(x, innerHeight / 2 + y);
+  }
+  c.stroke();
+};
+
+const adjustedSin = (x) => {
+  const amplitude = innerHeight / 2;
+  const frequency = (Math.PI * 2) / innerWidth;
+
+  return Math.sin(x * frequency) * -amplitude;
+};
+
 const animate = () => {
   requestAnimationFrame(animate);
 
   c.clearRect(0, 0, innerWidth, innerHeight);
-  // c.fillRect(100, 100, 100, 100);
+
+  // Amplitude is negative because canvas maps y positive in the down direction
+  graph(adjustedSin);
 };
 
 window.addEventListener("contextmenu", (event) => {
